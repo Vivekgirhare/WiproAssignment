a2dc628e91ee9a458a5633be1a25d74de2082f99
Vivek girhare <vivekgirhare123@gmail.com>
2025-08-09 08:36:34 +0530

    Add files via upload

---
+++ Zipkin/Zipkin_Zipkin/src/main/java/com/wipro/Zipkin/ZipkinApplication.java
@@ -0,0 +1,13 @@
+package com.wipro.Zipkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZipkinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZipkinApplication.class, args);
+	}
+
+}
